404dd858f75298235168bb782423ab13c32f340a
FeryaFox <bogdashkin145@gmail.com>
2025-04-12 21:49:28 +0300

    Revert "0.0.12 Исправлен парсинг при ошибке"
    This reverts commit 4330ac6c74d43a29f8e387c1ad791a2a2ebb12ff.

--- src/main/java/ru/feryafox/kavita4j/http/HttpClient.java
+++ src/main/java/ru/feryafox/kavita4j/http/HttpClient.java
@@ -113,26 +113,22 @@ public class HttpClient implements BaseHttpClient {
 
     private <T extends BaseKavitaResponseModel> HttpClientResponse<T> call(Request request, Class<T> clazz) {
         try (Response response = client.newCall(request).execute()) {
-            String responseBody = response.body() != null ? response.body().string() : "";
-
             if (response.isSuccessful()) {
                 if (clazz == NoneResponse.class) {
                     @SuppressWarnings("unchecked")
                     T noneInstance = (T) NoneResponse.create();
                     return HttpClientResponse.from(response, noneInstance);
                 }
-
-                try {
-                    T model = gson.fromJson(responseBody, clazz);
-                    return HttpClientResponse.from(response, model);
-                } catch (Exception parseException) {
-                    return HttpClientResponse.from(response, "Failed to parse response: " + responseBody);
-                }
-
+                return HttpClientResponse.from(
+                        response,
+                        gson.fromJson(response.body().string(), clazz)
+                );
             } else {
-                return HttpClientResponse.from(response, responseBody);
+                return HttpClientResponse.from(
+                        response,
+                        response.body().string()
+                );
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
